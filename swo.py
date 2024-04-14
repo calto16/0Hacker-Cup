40bc514d7d0c753e627a798a020ea8a617bcc5a7
calto16 <97183747+calto16@users.noreply.github.com>
2024-04-14 16:39:43 +0530

    Add files via upload

---
+++ swo.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+#add cors
+from flask_cors import CORS
+app = Flask(__name__)
+
+
+CORS(app)
+#allow all origins
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/',methods=['GET'])
+def home():
+    return "Hello World"
+
+
+@app.route('/api', methods=['POST'])
+def add_data():
+    try:
+        print(request.json)
+        return jsonify({"message":"Data received" + str(request.json)}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
